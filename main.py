5885428f1da623ec99bd125e8f00d6a10f4d259a
maxou-uoxam <57676020+maxou-uoxam@users.noreply.github.com>
2023-03-29 21:28:13 +0200

    correction requirements

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Import
 import streamlit as st
 import pandas as pd
-import lifelines
-import plotly.express as px
-import plotly.graph_objects as go
-import matplotlib as plt
-from streamlit_option_menu import option_menu
+# import lifelines
+# import plotly.express as px
+# import plotly.graph_objects as go
+# import matplotlib as plt
 import hydralit_components as hc
 from random import randint
 
